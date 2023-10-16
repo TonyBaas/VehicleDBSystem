047fa3039094d9ea240fede94b20465af8b669c9
tonybaas <tonyrrbaas@gmail.com>
2023-10-15 20:13:39 -0500

    Add project files.

---
+++ VehicleDBSystem/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VehicleDBSystem.Models;
+
+namespace VehicleDBSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
